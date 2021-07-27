2b27e2577042a024f6897e11a2122f5409da8412
jakelsaunders94 <jakelsaunders94@gmail.com>
2021-07-27 15:52:56 +0100

    Got a working navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import TodoNavbar from "./Components/navbar/navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "logged_in": false
+        }
+        this.navbarButtons = [
+            {
+                "text": "Home",
+                "onClick": () => console.log("Home Clicked")
+            },
+            {
+                "text": this.state.logged_in ? "Log Out" : "Log In",
+                "onClick": () => console.log("Log in / Out Clicked")
+            },
+            {
+                "text": "To-Do List",
+                "onClick": () => console.log("To-Do Clicked")
+            },
+            {
+                "text": "Reminders",
+                "onClick": () => console.log("Reminders Clicked")
+            }
+        ]
+    }
+
+    render() {
+        return (
+            <div>
+                <TodoNavbar homeText="Jakes To-Do List" buttons={this.navbarButtons}/>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
 }
-
-export default App;
---
+++ src/Components/navbar/navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Navbar, Container, Nav, NavDropdown} from "react-bootstrap";
+import "base.scss"
+
+export default function TodoNavbar(props) {
+    return (
+        <Navbar className="bg-todo-primary" expand="lg">
+            <Container className="w-100">
+                <Navbar.Brand onClick={() => props.homeClick()}>{props.homeText}</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        {props.buttons.map((button, index) => (
+                            <Nav.Link key={index} onClick={button.onClick}>{button.text}</Nav.Link>
+                        ))}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+TodoNavbar.propTypes = {
+    homeClick: PropTypes.string,
+    homeText: PropTypes.string.isRequired,
+    buttons: PropTypes.array.isRequired,
+    navLinkClick: PropTypes.func
+}
