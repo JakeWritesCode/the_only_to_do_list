ab481a62f4498bdf6c9db3d0d6c76dd04fcdb2b9
jakelsaunders94 <jakelsaunders94@gmail.com>
2021-09-06 11:00:34 +0100

    Refactored my signup form.

--- src/screens/signup/signup.js
+++ src/screens/signup/signup.js
@@ -1,7 +1,4 @@
-// The home splash funky bit
-
 import React from 'react';
-import PropTypes from 'prop-types';
 import "../screens_common.scss"
 import {ArrowBack} from "@material-ui/icons";
 import {useHistory} from "react-router-dom";
@@ -10,7 +7,6 @@ import {Formik, Field} from 'formik';
 
 const SignUpForm = () => {
     const history = useHistory()
-
     return (
         <Formik
             initialValues={{
@@ -20,7 +16,7 @@ const SignUpForm = () => {
                 password: "",
                 passwordConfirm: ""
             }}
-
+            validateOnChange={true}
             validate={(values) => {
                 let errors = {};
                 if (!values.firstName) {
@@ -40,22 +36,18 @@ const SignUpForm = () => {
                 }
                 return errors
             }}
-
             onSubmit={() => {
                 console.log('form submitted!!')
             }}
-
-            render={({
-                         handleChange,
-                         handleSubmit,
-                         handleBlur,
-                         values,
-                         errors
-                     }) => (
+        >
+             {({
+                  errors,
+                  touched,
+                  handleSubmit
+              }) => (
                 <Form>
-                    <Field
-                        name="email"
-                        render={({field, formProps}) => (
+                    <Field name="email">
+                        {({field, formProps}) => (
                             <Form.Group className="mb-3" controlId="email">
                                 <Form.Label>Email address</Form.Label>
                                 <Form.Control type="email"
@@ -63,16 +55,16 @@ const SignUpForm = () => {
                                               value={field.value}
                                               onChange={field.onChange}
                                 />
+                                {touched.email && errors.email &&
+                                <div className="form-error">{errors.email}</div>}
                                 <Form.Text className="text-muted">
                                     Well never share your email with anyone else.
                                 </Form.Text>
                             </Form.Group>
                         )}
-                    />
-
-                    <Field
-                        name="firstName"
-                        render={({field, formProps}) => (
+                    </Field>
+                    <Field name="firstName">
+                        {({field, formProps}) => (
                             <Form.Group className="mb-3"
                                         controlId="firstName">
                                 <Form.Label>First Name</Form.Label>
@@ -83,11 +75,9 @@ const SignUpForm = () => {
                                 />
                             </Form.Group>
                         )}
-                    />
-
-                    <Field
-                        name="lastName"
-                        render={({field, formProps}) => (
+                    </Field>
+                    <Field name="lastName">
+                        {({field, formProps}) => (
                             <Form.Group className="mb-3"
                                         controlId="lastName">
                                 <Form.Label>Last Name</Form.Label>
@@ -98,11 +88,9 @@ const SignUpForm = () => {
                                 />
                             </Form.Group>
                         )}
-                    />
-
-                    <Field
-                        name="password"
-                        render={({field, formProps}) => (
+                    </Field>
+                    <Field name="password">
+                        {({field, formProps}) => (
                             <Form.Group className="mb-3"
                                         controlId="password">
                                 <Form.Label>Password</Form.Label>
@@ -113,11 +101,9 @@ const SignUpForm = () => {
                                 />
                             </Form.Group>
                         )}
-                    />
-
-                    <Field
-                        name="passwordConfirm"
-                        render={({field, formProps}) => (
+                    </Field>
+                    <Field name="passwordConfirm">
+                        {({field, formProps}) => (
                             <Form.Group className="mb-3"
                                         controlId="passwordConfirm">
                                 <Form.Label>Confirm Password</Form.Label>
@@ -128,16 +114,14 @@ const SignUpForm = () => {
                                 />
                             </Form.Group>
                         )}
-                    />
-
+                    </Field>
                     <Button className="btn btn-lg btn-todo-primary" onClick={handleSubmit}>Sign
                         Up</Button>
                     <Button className="btn btn-lg btn-light float-end"
                             onClick={() => history.push("/login")}>Go to Log In</Button>
                 </Form>
-            )
-            }
-        />
+             )}
+        </Formik>
     )
 }
 
@@ -164,7 +148,7 @@ export default function SignupScreen(props) {
                             <Card className="w-75" style={{margin: "auto"}}>
                                 <Card.Body>
                                     <Card.Title className="mb-2">Sign Up Today!</Card.Title>
-                                    {SignUpForm()}
+                                    <SignUpForm/>
                                 </Card.Body>
                             </Card>
                         </div>
